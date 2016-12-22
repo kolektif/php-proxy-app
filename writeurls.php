66c54e7717bddc8233f617f2e79bf72cc6d2e1ca
shibby <guvenatbakan@gmail.com>
2016-12-22 04:30:19 +0300

    dir update

--- writeurls.php
+++ writeurls.php
@@ -1,4 +1,4 @@
 <?php
 
 $urlList = file_get_contents('https://raw.githubusercontent.com/kolektif/sansur-listesi/master/liste.json');
-file_put_contents(__DIR__.'url_list.json', $urlList);
+file_put_contents(__DIR__.'/url_list.json', $urlList);
